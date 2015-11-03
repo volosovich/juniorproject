64cd699a37686a46b2276ae305de3ff98670f952
Volosovich <andreyvolosovich@gmail.com>
2015-11-03 23:14:05 +0200

    add some feature

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,10 @@
-from .views import TestPage
+from .views import ProductPage, CategoryPage
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^$', TestPage.as_view(), name = 'index'),
+#    url(r'^$', TestPage.as_view(), name = 'index'),
+    url(r'^category/', CategoryPage.as_view(), name = 'categories'),
+    url(r'^', ProductPage.as_view(), name = 'products'),
+#    url(r'^category/', CategoryPage.as_view(), name = 'categories')
 ]
 #url(r'^$', TestPage.as_view(), name = 'index')
--- product/views.py
+++ product/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.views.generic import ListView
-from .models import TestforPage
+from .models import Product, Category
 
 
 
-class TestPage(ListView):
-    model = TestforPage
-    template_name = 'test-page.html'
+#class TestPage(ListView):
+#    model = TestforPage
+#    template_name = 'test-page.html'
 
-#def main(request):
-#    return render_to_response('', {})
+class ProductPage(ListView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name ='products'
+
+class CategoryPage(ListView):
+    model = Category
+    template_name = 'category.html'
+    context_object_name = 'categories'
